c6c7d4fa051f9d5924439cdbf2a98beb8ae05430
Daniel Carrasco <Daniel.Carrasco@jala.university>
2023-05-22 20:30:38 -0400

    add files to run tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
     transform: { '^.+\\.ts?$': 'ts-jest' },
     testEnvironment: 'node',
     testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    verbose: true,
+    reporters: [
+        "default",
+        ["./node_modules/jest-html-reporter", {
+        "pageTitle": "Report",
+        "outputPath": "./test/report/report.html"
+        }]
+     ]
 };
